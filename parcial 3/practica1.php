306ada172d3c6aa6a2640c186e5b91f5ebc07ce3
terrazasjuancarlos <124410875+terrazasjuancarlos@users.noreply.github.com>
2023-05-22 18:59:33 -0600

    Add files via upload

---
+++ parcial 3/practica1.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo"<h1>hola pepas/<h1>";
+    echo"<hr>";
+
+    $nombre ="juan";
+    $edad = 17;
+    echo"<br><br>";
+    if($edad>= 17){
+        echo $nombre . " es mayor que ";
+    }else{
+        echo $nombre . " es menor que ";
+    }
+    echo"<br><br>";
+
+    for($i=0; $i<=10; $i++ ){
+        echo $i . "<br>";
+    } 
+
+
+
+
+
+
+?>
