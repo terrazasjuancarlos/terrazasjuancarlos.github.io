1e352cb327804d36494157efef862d96518082b7
terrazasjuancarlos <124410875+terrazasjuancarlos@users.noreply.github.com>
2023-06-15 19:04:13 -0700

    Add files via upload

--- parcial 3/practica7.php
+++ parcial 3/practica7.php
@@ -21,25 +21,29 @@
         .azul { background-color:  rgb(1,173,239); }
         .morado { background-color: rgb(131,78,160); }
         .naranja { background-color: orange ; }
+        .snake{ background-imag : url ('snake.jpg'); }
     </style>
 </head>
 <body>
     <div class="container">
         <h1>Serpientes y Escaleras</h1>
-        <div class="row">
-            <?php
-            $colors = ['rojo', 'amarillo', 'verde', 'azul', 'morado','naranja'];
+            <form action="practica7.php" method="post">
+                <input type="submit" value="jugar" class = "btn btn-success">
+                    <div class="row">
+                        <?php
+                        $colors = ['rojo', 'amarillo', 'verde', 'azul', 'morado','naranja'];
 
-            for($i = 100; $i > 0; $i--) {
-                $randomColor = $colors[array_rand($colors)];
-                echo "<div class='col-1'><div class='casilla $randomColor'>$i</div></div>";
+                        for($i = 100; $i > 0; $i--) {
+                            $randomColor = $colors[array_rand($colors)];
+                            echo "<div class='col-1'><div class='casilla $randomColor'>$i</div></div>";
 
-                if($i==30) {
-                    echo "<img src='escalera de mano.Png' style='width: 8%;'>";
-                }
-            }
-            ?>
-        </div>
+                            if($i==30) {
+                                echo "<img src='escalera de mano.Png' style='width: 8%;'>";
+                            }
+                        }
+                        ?>
+                    </div>
+            </form>        
     </div>
 </body>
 </html>
